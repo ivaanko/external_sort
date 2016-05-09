d10ee24555835fbdabe1d7971efc120608a18601
Ivan Korolev <ivan.korolev@lazada.com>
2016-05-09 18:05:24 +0700

    trying to re-use read_into_buffer, there is a bug

--- external_sort.go
+++ external_sort.go
@@ -46,14 +46,21 @@ func read_into_buffer(input_buffer_ref *[]string, fhandler *os.File, chunk_size 
 	for (len(*input_buffer_ref) < chunk_size) && !eof {
 		eof = !scanner.Scan()
 		if eof {
+			if debug {
+				log.Println("DEBUG: eof")
+				log.Printf("[last]=%d\n", len(*input_buffer_ref))
+				if len(*input_buffer_ref) > 0 {
+					log.Printf("last=%d\n", (*input_buffer_ref)[len(*input_buffer_ref)-1])
+				}
+			}
 			if err := scanner.Err(); err != nil {
 				log.Println(err)
 			}
 			break
 		}
-		if debug {
+		/*if debug {
 			log.Printf("scanner.Text()=%v\n", scanner.Text())
-		}
+		}*/
 		*input_buffer_ref = append(*input_buffer_ref, scanner.Text())
 	}
 	if debug {
@@ -85,46 +92,35 @@ func main() {
 		log.Fatal(err)
 	}
 	//defer infile.Close()
+
 	/*
 	* 1. Read input files into chunks fitting available memory, sort the chunks
 	* using mysort() and dump them to set of intermediate files
 	 */
 	tot_lines := 0
-	eof := false
 	ind := 0 // intermediate file name index, also number of sorted pieces for further merge
-	scanner := bufio.NewScanner(infile)
-	for !eof {
+	for {
 		var lines []string
+		lines_read := read_into_buffer(&lines, infile, avail_mem-1)
 		if debug {
-			log.Printf("DEBUG: len(lines)=%d, avail_mem=%d\n", len(lines), avail_mem)
+			log.Println("read " + strconv.Itoa(lines_read) + " lines")
+			log.Printf("ind=%d\n", ind)
 		}
-		for len(lines) < (avail_mem - 1) {
-			if debug {
-				log.Printf("read %d lines\n", len(lines))
-			}
-			eof = !scanner.Scan()
-			if eof {
-				if err := scanner.Err(); err != nil {
-					log.Println(err)
-				}
-				break
-			}
-			lines = append(lines, scanner.Text())
+		if lines_read == 0 {
+			break
 		}
-		tot_lines += len(lines)
-		if len(lines) > 0 {
-			mysort(lines, 0, len(lines)-1)
-			out_fname := inter_fname_patt + "_" + strconv.Itoa(ind)
-			outfile, err := os.Create(out_fname)
-			if err != nil {
-				log.Fatal(err)
-			}
-			for i := 0; i < len(lines); i += 1 {
-				outfile.WriteString(lines[i] + "\n")
-			}
-			outfile.Close()
-			ind += 1
+		tot_lines += lines_read
+		mysort(lines, 0, lines_read-1)
+		out_fname := inter_fname_patt + "_" + strconv.Itoa(ind)
+		outfile, err := os.Create(out_fname)
+		if err != nil {
+			log.Fatal(err)
 		}
+		for i := 0; i < lines_read; i += 1 {
+			outfile.WriteString(lines[i] + "\n")
+		}
+		outfile.Close()
+		ind += 1
 	}
 	if debug {
 		log.Println("read total " + strconv.Itoa(tot_lines) + " lines")
@@ -161,9 +157,9 @@ func main() {
 		read_into_buffer(&input_buffers[i], fhandlers[i], chunk_size)
 	}
 
-	if debug {
+	/*if debug {
 		log.Printf("input_buffers=%v\n", input_buffers)
-	}
+	}*/
 	empty_handlers := 0
 	// top-level loop here
 	for {
@@ -172,17 +168,17 @@ func main() {
 		min_ind := 0
 		// skip empty buffer
 		for min_ind = 0; min_ind < ind; min_ind += 1 {
-			if debug {
+			/*if debug {
 				log.Printf("min_ind=%d\n", min_ind)
 				log.Printf("len(input_buffers[min_ind])=%d\n", len(input_buffers[min_ind]))
-			}
+			}*/
 			if len(input_buffers[min_ind]) > 0 {
 				break
 			}
 		}
-		if debug {
+		/*if debug {
 			log.Printf("min_ind=%d\n", min_ind)
-		}
+		}*/
 		for i := min_ind + 1; i < ind; i += 1 {
 			// skip empty buffer
 			if len(input_buffers[i]) == 0 {
@@ -193,9 +189,9 @@ func main() {
 				min_ind = i
 			}
 		}
-		if debug {
+		/*if debug {
 			log.Printf("min_ind=%d\n", min_ind)
-		}
+		}*/
 		// shift
 		x := input_buffers[min_ind][0]
 		input_buffers[min_ind] = input_buffers[min_ind][1:]
