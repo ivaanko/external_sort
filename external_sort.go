a2ab70013626aa75f554f0e254a172fc4a89c1b6
Ivan Korolev <ivan.korolev@lazada.com>
2016-05-09 16:48:52 +0700

    finally translated to go

--- external_sort.go
+++ external_sort.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 )
 
+var debug bool
+
 /* okay make my own sort to avoid using std one
 accepts list reference to avoid list copy
 and say let it be classic quicksort */
@@ -35,17 +37,48 @@ func mysort(array_ref []string, lo int, hi int) {
 		mysort(array_ref, p+1, hi)
 	}
 }
+func read_into_buffer(input_buffer_ref *[]string, fhandler *os.File, chunk_size int) int {
+	scanner := bufio.NewScanner(fhandler)
+	eof := false
+	if debug {
+		log.Printf("len(*input_buffer_ref)=%d, chunk_size=%d\n", len(*input_buffer_ref), chunk_size)
+	}
+	for (len(*input_buffer_ref) < chunk_size) && !eof {
+		eof = !scanner.Scan()
+		if eof {
+			if err := scanner.Err(); err != nil {
+				log.Println(err)
+			}
+			break
+		}
+		if debug {
+			log.Printf("scanner.Text()=%v\n", scanner.Text())
+		}
+		*input_buffer_ref = append(*input_buffer_ref, scanner.Text())
+	}
+	if debug {
+		log.Printf("len(*input_buffer_ref)=%d\n", len(*input_buffer_ref))
+	}
+	return len(*input_buffer_ref)
+}
 
 func main() {
 	/* I make an assumption that we measure memory in lines.
 	I could set this limit in bytes, look through the input file with one more pass
 	to find the longest line length but I think we can omit this here. */
 	var avail_mem int
-	var input_file, output_file string
 	flag.IntVar(&avail_mem, "avail_mem", 100000, "Number of lines to fit the memory")
-	flag.StringVar(&input_file, "input_file", "", "Input file path")
-	flag.StringVar(&output_file, "output_file", "", "Output file path")
+	flag.BoolVar(&debug, "debug", false, "Pring debug info")
 	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		log.Fatal("Usage: " + os.Args[0] + " [-avail_mem <lines>] <input file> <output file>")
+	}
+	input_file := args[0]
+	output_file := args[1]
+	if debug {
+		log.Println("DEBUG: debug is on")
+	}
 	inter_fname_patt := "mysorted" // pattern to name intermediate files
 	infile, err := os.Open(input_file)
 	if err != nil {
@@ -61,8 +94,14 @@ func main() {
 	ind := 0 // intermediate file name index, also number of sorted pieces for further merge
 	scanner := bufio.NewScanner(infile)
 	for !eof {
-		lines := make([]string, avail_mem)
-		for len(lines) < avail_mem {
+		var lines []string
+		if debug {
+			log.Printf("DEBUG: len(lines)=%d, avail_mem=%d\n", len(lines), avail_mem)
+		}
+		for len(lines) < (avail_mem - 1) {
+			if debug {
+				log.Printf("read %d lines\n", len(lines))
+			}
 			eof = !scanner.Scan()
 			if eof {
 				if err := scanner.Err(); err != nil {
@@ -70,7 +109,7 @@ func main() {
 				}
 				break
 			}
-			lines[len(lines)] = scanner.Text()
+			lines = append(lines, scanner.Text())
 		}
 		tot_lines += len(lines)
 		if len(lines) > 0 {
@@ -81,85 +120,113 @@ func main() {
 				log.Fatal(err)
 			}
 			for i := 0; i < len(lines); i += 1 {
-				outfile.WriteString(lines[i])
+				outfile.WriteString(lines[i] + "\n")
 			}
 			outfile.Close()
 			ind += 1
 		}
 	}
-	log.Println("read total " + strconv.Itoa(tot_lines) + " lines")
+	if debug {
+		log.Println("read total " + strconv.Itoa(tot_lines) + " lines")
+		log.Printf("ind=%d\n", ind)
+	}
 	infile.Close()
-	/*
-	   ## 2. Merge the intermediate files into output file
-	   open (OUT, ">$output_file") or die "Could not create output file '$output_file': $!";
-	   my @fhandlers;
-	   for (my $i = 0; $i < $ind; ++$i) {
-	     my $fname = "${inter_fname_patt}_$i";
-	     open ($fhandlers[$i], $fname) or die "Could not open intermediate file '$fname': $!";
-	   }
 
-	   my $chunk_size = int($avail_mem / ($ind + 1)); ## +1 for output buffer
-	   print "chunk_size='$chunk_size'\n";
-	   my @output_buffer;
-	   my @input_buffers;
+	// 2. Merge the intermediate files into output file
+	outfile, err := os.Create(output_file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
+	var fhandlers []*os.File
+	for i := 0; i < ind; i += 1 {
+		fname := inter_fname_patt + "_" + strconv.Itoa(i)
+		fh, err := os.Open(fname)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer fh.Close()
+		fhandlers = append(fhandlers, fh)
+	}
+	chunk_size := int((avail_mem - 1) / (ind + 1)) // -1 for current line, +1 for output buffer
+	if debug {
+		log.Printf("chunk_size=%d\n", chunk_size)
+	}
 
-	   sub read_into_buffer {
-	     my ($i, $input_buffers_ref, $fhandler, $chunk_size) = @_;
-	     my @lines;
-	     my $line;
-	     while ((@lines < $chunk_size) and defined ($line = <$fhandler>)) {
-	       push @lines, $line;
-	     }
-	     $input_buffers_ref->[$i] = \@lines;
-	     return int(@lines);
-	   }
+	var output_buffer []string
+	var input_buffers [][]string
+	input_buffers = make([][]string, ind)
+	// read into input buffers
+	for i := 0; i < ind; i += 1 {
+		read_into_buffer(&input_buffers[i], fhandlers[i], chunk_size)
+	}
 
-	   ## read into input buffers
-	   for (my $i = 0; $i < $ind; ++$i) {
-	     read_into_buffer($i, \@input_buffers, $fhandlers[$i], $chunk_size);
-	   }
-	   my $empty_handlers = 0;
-	   ## top-level loop here
-	   do {
-	     ## k-merge here (ind-merge)
-	     ## find "minimum" of ind heads, put it into output buffer and pop it
-	     my $min_ind;
-	     ## skip empty buffer
-	     for ($min_ind = 0; $min_ind < $ind; ++$min_ind) {
-	       if (@{$input_buffers[$min_ind]}) {
-	         last;
-	       }
-	     }
-	     for (my $i = $min_ind + 1; $i < $ind; ++$i) {
-	       ## skip empty buffer
-	       next unless (@{$input_buffers[$i]});
-	       ## ok I won't implement my own string comparison
-	       if ($input_buffers[$i][0] lt $input_buffers[$min_ind][0]) {
-	         $min_ind = $i;
-	       }
-	     }
-	     push @output_buffer, shift @{$input_buffers[$min_ind]};
-	     if (@output_buffer == $chunk_size) {
-	       ## flush output buffer
-	       print OUT @output_buffer;
-	       @output_buffer = ();
-	     }
-	     unless (@{$input_buffers[$min_ind]}) {
-	       unless (read_into_buffer($min_ind, \@input_buffers, $fhandlers[$min_ind], $chunk_size)) {
-	         ++$empty_handlers;
-	       }
-	     }
-	   } while ($empty_handlers < $ind);
+	if debug {
+		log.Printf("input_buffers=%v\n", input_buffers)
+	}
+	empty_handlers := 0
+	// top-level loop here
+	for {
+		// k-merge here (ind-merge)
+		// find "minimum" of ind heads, put it into output buffer and pop it
+		min_ind := 0
+		// skip empty buffer
+		for min_ind = 0; min_ind < ind; min_ind += 1 {
+			if debug {
+				log.Printf("min_ind=%d\n", min_ind)
+				log.Printf("len(input_buffers[min_ind])=%d\n", len(input_buffers[min_ind]))
+			}
+			if len(input_buffers[min_ind]) > 0 {
+				break
+			}
+		}
+		if debug {
+			log.Printf("min_ind=%d\n", min_ind)
+		}
+		for i := min_ind + 1; i < ind; i += 1 {
+			// skip empty buffer
+			if len(input_buffers[i]) == 0 {
+				continue
+			}
+			// ok I won't implement my own string comparison
+			if input_buffers[i][0] < input_buffers[min_ind][0] {
+				min_ind = i
+			}
+		}
+		if debug {
+			log.Printf("min_ind=%d\n", min_ind)
+		}
+		// shift
+		x := input_buffers[min_ind][0]
+		input_buffers[min_ind] = input_buffers[min_ind][1:]
+		output_buffer = append(output_buffer, x)
+		if len(output_buffer) == chunk_size {
+			// flush output buffer
+			for i := 0; i < len(output_buffer); i += 1 {
+				outfile.WriteString(output_buffer[i] + "\n")
+			}
+			output_buffer = nil
+		}
+		if len(input_buffers[min_ind]) == 0 {
+			if 0 == read_into_buffer(&input_buffers[min_ind], fhandlers[min_ind], chunk_size) {
+				empty_handlers += 1
+			}
+		}
+		if empty_handlers == ind {
+			break
+		}
+	}
 
-	   ## final flush of out buffer
-	   print OUT @output_buffer;
-	   close OUT;
+	// final flush of out buffer
+	for i := 0; i < len(output_buffer); i += 1 {
+		outfile.WriteString(output_buffer[i] + "\n")
+	}
+	outfile.Close()
 
-	   ## cleanup
-	   for (my $i = 0; $i < $ind; ++$i) {
-	     my $fname = "${inter_fname_patt}_$i";
-	     close ($fhandlers[$i]) or die "Could not close intermediate file '$fname': $!";
-	     unlink $fname or die "Could not delete intermediate file '$fname': $!";
-	   }
-	*/
+	// cleanup
+	for i := 0; i < ind; i += 1 {
+		fname := inter_fname_patt + "_" + strconv.Itoa(ind)
+		fhandlers[i].Close()
+		os.Remove(fname)
+	}
 }
